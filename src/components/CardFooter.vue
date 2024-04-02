88338dfaea35f335a2e1e4c790ca44b6bc33b404
syvhix <nicolasbalthaze@gmail.com>
2024-04-02 13:21:48 +0200

    third commit

--- src/components/CardFooter.vue
+++ src/components/CardFooter.vue
@@ -2,10 +2,10 @@
     <div>
       <hr class="border-1 my-8 border-gray-400 sm:w-4/12 mx-auto">
       <div class="text-center">
-        <a href="#" target="_blank"
+        <a href="https://www.linkedin.com/" target="_blank"
           class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
             icon="fa-brands fa-linkedin" /></a>
-        <a href="#" target="_blank"
+        <a href="https://www.facebook.com/" target="_blank"
           class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
             icon="fa-brands fa-facebook" /></a>
         <a href="https://www.instagram.com/syvhix._/" target="_blank"
